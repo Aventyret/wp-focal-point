89c9b9801baf3c328b4ed4172a8342b774b485c3
Clara Collman <70628192+claracollman@users.noreply.github.com>
2023-06-21 11:26:20 +0200

    initial styling and javascript click functions

--- public/hooks.php
+++ public/hooks.php
@@ -1,6 +1,8 @@
 <?php
 add_action( 'admin_enqueue_scripts', function() {
   wp_enqueue_script( 'wp-focal-point', WP_FOCAL_POINT_URI . 'public/wp-focal-point.js', NULL, WP_FOCAL_POINT_VER, true );
+
+  wp_enqueue_style( 'wp-focal-point', WP_FOCAL_POINT_URI . 'public/wp-focal-point.css', NULL, WP_FOCAL_POINT_VER, 'all' );
 });
 
 add_filter( 'attachment_fields_to_edit', 'Wp_focal_point_fields::edit_focal_point_button', 10, 2);
--- public/wp-focal-point.js
+++ public/wp-focal-point.js
@@ -4,7 +4,7 @@
   }
 
   function closeModal() {
-    document.getElementById('cpt_Modal').remove();
+    document.getElementById('fpt_Modal').remove();
   }
 
   function maybeOpenModal(e) {
@@ -15,21 +15,64 @@
         return;
       }
       let modalHtml = '';
-      modalHtml+= '<div id="cpt_Modal" class="cpt_Modal">';
-      modalHtml+= '<div class="cpt_ModalDialog" role="dialog" aria-label="Set focal point">';
-      modalHtml+= '<button type="button" class="cpt_ModalClose" aria-label="close">&times;</button>';
-      modalHtml+= '<div class="cpt_ModalHeader"><div class="cpt_ModalTitle">Set focal point</div></div>';
-      modalHtml+= '<div class="cpt_ModalContent" id="cpt_crop_editor">';
+      modalHtml+= '<div id="fpt_Modal" class="fpt_Modal">';
+      modalHtml+= '<div class="fpt_ModalDialog" role="dialog" aria-label="Set focal point">';
+      modalHtml+= '<div class="media-frame-title"><h1>Attachment details</h1>';
+      modalHtml+= '<button type="button" class="fpt_ModalClose media-modal-close" aria-label="close">&times;</button></div>';
+      modalHtml+= '<div class="fpt_ModalContent" id="fpt_crop_editor">';
+      modalHtml+= '<div class="fpt_ModalColLeft">';
+      modalHtml+= '<div class="fpt_ModalHeader"><div class="fpt_ModalTitle">Set focal point</div></div>';
 
-      modalHtml += '<figure class="wp-focal-point-image"><img src="' + image.attributes.url + '" /></figure>';
+      // image
+      modalHtml += '<figure class="wp-focal-point-image"><div class="pos-wrapper"><p class="pos">X: <span class="posX">50</span>%</p><p class="pos">Y: <span class="posY">50</span>%</p>';
+      modalHtml += '</div><div class="fpt_cursor"></div><img src="' + image.attributes.url + '" /></figure>';
+      modalHtml += '<button class="button">Save</button>';
+      modalHtml+= '</div>'; // modalColLeft
 
-      modalHtml+= '</div>';//end cpt_ModalContent
-      modalHtml+= '</div>';//end cpt_ModalDialog
-      modalHtml+= '</div>';//end cpt_Modal;
+      // examples
+      modalHtml+= '<div class="fpt_ModalColRight">';
+      modalHtml+= '<div class="fpt_ModalTitle">Examples of crops</div>';
+
+      modalHtml+= '<div class="fpt-preview-wrap">';
+      modalHtml += '<figure class="fpt-preview fpt-preview-square"><img src="' + image.attributes.url + '" /></figure>';
+      modalHtml += '<figure class="fpt-preview fpt-preview-wide"><img src="' + image.attributes.url + '" /></figure>';
+      modalHtml += '<figure class="fpt-preview fpt-preview-portrait"><img src="' + image.attributes.url + '" /></figure>';
+      modalHtml+= '</div>';
+
+      modalHtml+= '</div>'; // modalColRight
+
+      modalHtml+= '</div>';
+      modalHtml+= '</div>';
+      modalHtml+= '</div>';
 
       document.body.insertAdjacentHTML('beforeend', modalHtml);
-      document.querySelector('.cpt_ModalClose').addEventListener('click', closeModal);
+      // document.body.appendChild(modalHtml);
+      document.querySelector('.fpt_ModalClose').addEventListener('click', closeModal);
+      document.querySelector('.wp-focal-point-image').addEventListener('click', function(e) {
+        setFocalPoint(e);
+      });
     }
+  }
+
+  function setFocalPoint (e) {
+    const imageEl = document.querySelector('.wp-focal-point-image');
+    const imageHeight = imageEl.offsetHeight;
+    const imageWidth = imageEl.offsetWidth;
+    const cursor = document.querySelector('.fpt_cursor');
+    const previewEls = document.querySelectorAll('.fpt-preview img');
+    let rect = imageEl.getBoundingClientRect();
+    let posX;
+    let posY;
+
+    posX = Math.round(((e.pageX - rect.left) / imageWidth) * 100);
+    posY = Math.round(((e.pageY - rect.top) / imageHeight) * 100);
+
+    document.querySelector('.posX').innerHTML = posX;
+    document.querySelector('.posY').innerHTML = posY;
+    cursor.style.top = posY + '%';
+    cursor.style.left = posX + '%';
+
+    previewEls.forEach(el => el.style.objectPosition = `${posX}% ${posY}%`);
   }
 
   eventListeners();
